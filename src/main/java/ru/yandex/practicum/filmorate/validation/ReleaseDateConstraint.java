f54569179d8e6405da8de19a10b6062fc2e83ec5
alina <alinagdl1@yandex.ru>
2025-04-21 22:34:34 +0500

    изменения8

--- src/main/java/ru/yandex/practicum/filmorate/validation/ReleaseDateConstraint.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/ReleaseDateConstraint.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.validation;
 
 import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
 
 import java.lang.annotation.*;
 
@@ -11,8 +10,4 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ReleaseDateConstraint {
     String message() default "Дата релиза не может быть раньше 28 декабря 1895";
-
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
 }
