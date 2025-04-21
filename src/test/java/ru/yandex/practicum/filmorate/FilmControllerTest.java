5e625d85341f7600a5e5c1853842c00169adc3cd
alina <alinagdl1@yandex.ru>
2025-04-21 22:47:11 +0500

    изменения8

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -10,6 +10,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import org.springframework.util.ResourceUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = ru.yandex.practicum.filmorate.FilmorateApplication.class)
@@ -46,6 +53,18 @@ class FilmControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+
+    private String getContentFromFile(String filename) {
+        try {
+            var resource = ResourceUtils.getFile("classpath:" + filename);
+            if (!resource.exists()) {
+                throw new RuntimeException("Файл не найден: " + filename);
+            }
+            return Files.readString(resource.toPath(), StandardCharsets.UTF_8);
+        } catch (IOException exception) {
+            throw new RuntimeException("Не открывается файл: " + filename, exception);
+        }
+    }
 }
 
 
