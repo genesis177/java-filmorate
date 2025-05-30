1e1b3a8389060ac8db5c095aa9c7a38b3dd944de
alina <alinagdl1@yandex.ru>
2025-05-30 20:23:29 +0500

    23

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -39,12 +39,14 @@ public class FilmControllerTest {
     public void setup() throws Exception {
         try (Connection conn = dataSource.getConnection();
              Statement stmt = conn.createStatement()) {
-            // Очистка таблиц
             stmt.execute("DELETE FROM FILM_GENRES");
             stmt.execute("DELETE FROM FILMS");
             stmt.execute("DELETE FROM GENRES");
+            stmt.execute("DELETE FROM MPA");
             // вставляем жанры
-            stmt.execute("INSERT INTO GENRES (id, name) VALUES (1, 'Комедия'), (2, 'Драма'), (3, 'Триллер'), (4, 'Боевик'), (5, 'Мелодрама')");
+            stmt.execute("MERGE INTO GENRES (id, name) VALUES (1, 'Комедия'), (2, 'Драма'), (3, 'Триллер'), (4, 'Боевик'), (5, 'Мелодрама');");
+            // вставляем MPA
+            stmt.execute("MERGE INTO MPA (id, name) VALUES (1, 'G'), (2, 'PG'), (3, 'PG-13'), (4, 'R'), (5, 'NC-17');");
         }
     }
 
